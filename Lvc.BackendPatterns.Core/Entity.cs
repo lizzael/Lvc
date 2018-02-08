b4b46c5c7cc1ef3046032397d4a6fae149c4f255
Lizzael Villar Cruz <lvillarcruz@bm1.brandsmart.com>
2018-02-08 12:53:41 -0500

    Add Entity.

---
+++ Lvc.BackendPatterns.Core/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lvc.BackendPatterns.Core
+{
+	public class Entity<TId> : IEquatable<Entity<TId>>
+	{
+		public TId Id { get; protected set; }
+
+		protected Entity() {}
+
+		public Entity(TId id) {
+			if (object.Equals(id, default(TId)))
+			{
+				var paramName = nameof(id);
+				throw new ArgumentException($"{paramName} can not have the default value", paramName);
+			}
+			Id = id;
+		}
+
+		public override int GetHashCode() =>
+			Id.GetHashCode();
+
+		public override bool Equals(object obj)
+		{
+			var entity = obj as Entity<TId>;
+			return entity == null
+				? false
+				: Equals(entity);
+		}
+
+		public bool Equals(Entity<TId> entity) =>
+			object.Equals(Id, entity.Id);
+	}
+}
