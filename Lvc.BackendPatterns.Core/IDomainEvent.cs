e4c0a39f2537d0f5a23c5fd2ba71c12b960eb4d1
Lizzael Villar Cruz <lvillarcruz@bm1.brandsmart.com>
2018-02-12 14:04:09 -0500

    Add IDomainEvent.

---
+++ Lvc.BackendPatterns.Core/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lvc.BackendPatterns.Core
+{
+	public interface IDomainEvent
+	{
+		DateTime DateTimeOccurred { get; }
+	}
+}
