5d3de28bc9d53ce29a574f95cd50c84dfb93ca3e
Lizzael Villar Cruz <lvillarcruz@bm1.brandsmart.com>
2018-02-12 14:40:49 -0500

    Add Events and Handlers folders.

--- Lvc.BackendPatterns.Core/Events/IDomainEvent.cs
+++ Lvc.BackendPatterns.Core/Events/IDomainEvent.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lvc.BackendPatterns.Core
+namespace Lvc.BackendPatterns.Core.Events
 {
 	public interface IDomainEvent
 	{
--- Lvc.BackendPatterns.Core/Handlers/IHandle.cs
+++ Lvc.BackendPatterns.Core/Handlers/IHandle.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lvc.BackendPatterns.Core.Events;
 
-namespace Lvc.BackendPatterns.Core
+namespace Lvc.BackendPatterns.Core.Handlers
 {
 	public interface IHandle<T>
 		where T : IDomainEvent
