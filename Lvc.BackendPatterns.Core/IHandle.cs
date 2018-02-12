751f5b9b9bd7f7f6fc1e2de6e4f6019db5f17058
Lizzael Villar Cruz <lvillarcruz@bm1.brandsmart.com>
2018-02-12 14:34:49 -0500

    Add IHandle.

---
+++ Lvc.BackendPatterns.Core/IHandle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lvc.BackendPatterns.Core
+{
+	public interface IHandle<T>
+		where T : IDomainEvent
+	{
+		void Handle(T t);
+	}
+}
