42c0264f6a65e29ea85971b8f4feba7be121a675
Lizzael Villar Cruz <lvillarcruz@bm1.brandsmart.com>
2018-02-13 15:04:49 -0500

    Add DomainEvents.

---
+++ Lvc.BackendPatterns.Core/Events/DomainEvents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lvc.BackendPatterns.Core.Handlers;
+
+namespace Lvc.BackendPatterns.Core.Events
+{
+	public static class DomainEvents
+	{
+		[ThreadStatic]
+		private static LinkedList<Delegate> _actions;
+
+		static DomainEvents()
+		{
+			_actions = new LinkedList<Delegate>();
+		}
+
+		public static void Register<TDomainEvent>(Action<TDomainEvent> callback)
+			where TDomainEvent : IDomainEvent
+		=>
+			_actions.AddLast(callback);
+
+		public static void ClearCallBacks() =>
+			_actions.Clear();
+
+		public static void Raise<TDomainEvent>(TDomainEvent domainEvent)
+			where TDomainEvent : IDomainEvent
+		{
+			var selectedActions = 
+				_actions
+				.Select(s => s as Action<TDomainEvent>)
+				.Where(w => w != null);
+			foreach (var action in selectedActions)
+				action(domainEvent);
+		}
+	}
+}
