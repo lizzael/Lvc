dd064e9aa11e039acba1b6b248a695111b983174
Lizzael Villar <lizzael.v.c@gmail.com>
2018-02-27 13:19:35 -0500

    Refactor DomainEventsDispatcher.

--- Lvc.BackendPatterns.Core/Events/DomainEventsDispatcher.cs
+++ Lvc.BackendPatterns.Core/Events/DomainEventsDispatcher.cs
@@ -11,7 +11,7 @@ namespace Lvc.BackendPatterns.Core.Events
 	public static class DomainEventsDispatcher
 	{
 		[ThreadStatic]
-		private static List<Type> _staticHandlers;
+		private static List<Type> _handlers;
 		
 		/// <summary>
 		/// Call this method on the "StartApp".
@@ -22,24 +22,31 @@ namespace Lvc.BackendPatterns.Core.Events
 				.GetExecutingAssembly()
 				.GetTypes();
 
-			_staticHandlers =
+			_handlers =
 				types
 				.Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IHandler<>)))
 				.ToList();
 		}
 
-		public static void Dispatch<TDomainEvent>(TDomainEvent domainEvent)
-			where TDomainEvent : IDomainEvent
+		public static void Dispatch(IDomainEvent domainEvent)
 		{
-			var iHandlerType = typeof(IHandler<>);
-			var selectedStaticHandlers =
-				_staticHandlers
-				.Where(h => iHandlerType.IsAssignableFrom(h));
-			foreach (var handler in selectedStaticHandlers)
+			bool IsRequiredHandler(Type @interface) =>
+				@interface.IsGenericType &&
+				@interface.GetGenericTypeDefinition() == typeof(IHandler<>)
+				&& @interface.GetGenericArguments()[0] == domainEvent.GetType();
+
+			bool CanHandle(Type type) =>
+				type
+				.GetInterfaces()
+				.Any(IsRequiredHandler);
+
+			var validHandlers = _handlers.Where(CanHandle);
+			foreach (var handlerType in validHandlers)
 			{
-				var instance = (IHandler<TDomainEvent>)Activator.CreateInstance(handler);
+				var instance = (IHandler<IDomainEvent>)Activator.CreateInstance(handlerType);
 				instance.Handle(domainEvent);
 			}
 		}
+
 	}
 }
